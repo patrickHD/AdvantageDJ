ae279e0cab596d452b2c51a4e8394f0c0461684b
Patrick H Daniel Jr <patrickhdaniel@outlook.com>
2020-06-18 14:33:56 -0500

    Add project files.

---
+++ AdvDjVs/Adv_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\pdaniel\Desktop\Personal\AdvDjVs\AdvDjVs\Adv_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
