eacfafeb223c543f8f552a9d38566514c85927c9
Patrick H Daniel Jr <patrickhdaniel@outlook.com>
2020-06-18 15:51:15 -0500

    add static url

--- AdvDjVs/AdvDjVs/urls.py
+++ AdvDjVs/AdvDjVs/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,4 +29,4 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
